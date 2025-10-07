f15e9cc4bb75771747bcfc85fbac569a5349b750
marta <m.nowak033@gmail.com>
2025-10-07 19:44:52 +0200

    Fix issue with overflow on homepage

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,7 +1,7 @@
 <template>
   <article>
-    <img :src="eventData.image" :alt="eventData.imageAlt" class="max-w-96 h-auto rounded-lg" />
-    <h2 class="text-2xl max-w-96 text-center">{{ eventData.eventName }}</h2>
+    <img :src="eventData.image" :alt="eventData.imageAlt" class="h-auto rounded-lg" />
+    <h2 class="text-2xl text-center">{{ eventData.eventName }}</h2>
   </article>
 </template>
 
