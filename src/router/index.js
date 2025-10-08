49f1a9be5aae19ca2d6b21dc90b725bae5ef8f37
shoaibalirts <shoaibali.rts@gmail.com>
2025-10-08 14:34:35 +0200

    created Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,13 @@
 import App from "@/App.vue";
 import { createRouter, createWebHistory } from "vue-router";
-const routes = [{ path: "/", component: App }];
+const routes = [
+  { path: "/", name: "home", component: App },
+  {
+    path: "/events",
+    name: "events",
+    component: App,
+  },
+];
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
